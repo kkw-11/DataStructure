f805fc814f62660e23dd01cb26b9406c34acc26a
asus <kiwon1011@naver.com>
2020-12-29 15:53:26 +0900

    12/29 2차시

--- DataStructure/ex2.cpp
+++ DataStructure/ex2.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
-
+int garr[10];
 int main() {
 
-	printf("ex2\n"); 
+	int arr[10];
 
-	return 0;
+	return 0; 
 }
+
+
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int n = 10;
+//	int m = 20;
+//
+//	printf("%d %d\n", n, m);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//
+//	printf("ex2\n"); 
+//
+//	return 0;
+//}

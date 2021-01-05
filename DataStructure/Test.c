d4491a0dc4741ecea84ea6b1f8c990a1a944e360
asus <kkw-11@naver.com>
2021-01-05 11:58:59 +0900

    exercise

--- DataStructure/Test.c
+++ DataStructure/Test.c
@@ -19,10 +19,39 @@ int main() {
 
 	for (int i = 1; i <= T; ++i) {
 		printf("Input number:");
-		scanf("%d",&num);
+		scanf("%d", &num);
 		value = factorial(num);
-		printf("#%d %d! = %lld\n",i,num,value);
+		printf("#%d %d! = %lld\n", i, num, value);
 	}
 
 	return 0;
 }
+
+//#include <stdio.h>
+//
+//long long factorial(int n) {
+//
+//	if (n == 0)
+//		return 1;
+//	else
+//		return n * factorial(n - 1);
+//}
+//
+//
+//int main() {
+//
+//	int T;
+//	int num;
+//	long long value;
+//	printf("Input test count");
+//	scanf("%d", &T);
+//
+//	for (int i = 1; i <= T; ++i) {
+//		printf("Input number:");
+//		scanf("%d",&num);
+//		value = factorial(num);
+//		printf("#%d %d! = %lld\n",i,num,value);
+//	}
+//
+//	return 0;
+//}

96d44a06a5fb8b60f447eb02afe880a880f12305
asus <kkw-11@naver.com>
2021-01-06 20:55:14 +0900

    1/6 function sequence

--- DataStructure/ex6.cpp
+++ DataStructure/ex6.cpp
@@ -1,7 +1,105 @@
 #include <stdio.h>
+void Print(int n) {
+
+	printf("%d \n",n);
+	Print(n); 
+}
 
 int main() {
 
-
+	Print(3);
 	return 0;
-} 
+}
+
+
+//#include <stdio.h>
+//
+//void a() {
+//
+//}
+//
+//void b() {
+//	a();
+//}
+//
+//void c() {
+//	a();
+//	b();
+//}
+//
+//int main() {
+//
+//	a();
+//	b(); 
+//	c();
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//void Print2(int n) {
+//	printf("Print2: %d\n", n);
+//
+//}
+//
+//void Print3(int n) {
+//
+//	printf("Print3 : %d\n", n);
+//
+//}
+//void Print(int n) {
+//
+//	printf("Print : %d\n", n);
+//
+//	Print2(4);
+//	printf("Print2 finish : %d\n");
+//
+//	Print3(5);
+//	printf("Print3 finish : %d\n");
+//
+//	printf("Print finish\n");
+//}
+//
+//int main() {
+//
+//	Print(3);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//
+//
+//void Print2(int n) {
+//
+//	printf("Print2: %d\n", n);
+//
+//}
+//
+//void Print3(int n) {
+//
+//	 printf("Print3 : %d\n", n);
+//
+//}
+//void Print(int n) {
+//
+//	printf("Print : %d\n", n);
+//
+//	Print2(4);
+//	printf("Print2 finish : %d\n");
+//
+//	Print3(5);
+//	printf("Print3 finish : %d\n");
+//
+//	printf("Print finish\n");
+//}
+//
+//int main() {
+//
+//	Print(3);
+//
+//	return 0;
+//}
+

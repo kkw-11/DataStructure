a387af6485b9c04a08e3c9cff859c1374dd3c96e
asus <kkw-11@naver.com>
2021-07-29 20:14:35 +0900

    Tree

--- DataStructure/ex13.cpp
+++ DataStructure/ex13.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct _node{
+typedef struct _node {
 
 	int data;
 	struct _node* lChild;
@@ -14,7 +13,7 @@ Node* AllocNode(int data) {
 
 	Node* n = (Node*)malloc(sizeof(Node));
 	n->data = data;
-	n->lChild = n->lChild = NULL;
+	n->lChild = n->rChild = NULL;
 
 	return n;
 }
@@ -29,19 +28,40 @@ void Inorder(Node* root) {
 
 }
 
+void Preorder(Node* root) {
+	if (root == NULL)
+		return;
+	printf("%d ", root->data);
+	Preorder(root->lChild);
+	Preorder(root->rChild);
+}
+
+void Postorder(Node* root) {
+	if (root == NULL)
+		return;
+	Postorder(root->lChild);
+	Postorder(root->rChild);
+	printf("%d ", root->data);
+
+}
+
 int main() {
 
 	Node* root = NULL;
+
 	root = AllocNode(50);
+
 	root->lChild = AllocNode(30);
 	root->rChild = AllocNode(70);
 
 	root->lChild->lChild = AllocNode(10);
 	root->lChild->rChild = AllocNode(40);
+
 	root->rChild->lChild = AllocNode(60);
 	root->rChild->rChild = AllocNode(90);
 
-	Inorder(root); printf("\n");
+	Inorder(root);	printf("\n");
+	Preorder(root); print("\n");
 
 
 	return 0;
